34b1f9f5c22b6862c1fcb1a18f39752c0fc1e51e
marciondemelo <marciondemelo@gmail.com>
2022-02-17 22:50:49 -0300

    adicionado projeto com uma task fazendo acesso a uma classe que consulta uma api

---
+++ learn/implementation/learn-camunda-with-javaclass-readapi/src/main/java/com/example/workflow/Taxa.java
@@ -0,0 +1,15 @@
+package com.example.workflow;
+
+import lombok.Data;
+
+public class Taxa {
+    private double valor;
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}
---
+++ learn/implementation/learn-camunda-with-javaclass-readapi/src/main/java/com/example/workflow/event/ValorTotalJurosMesDelegate.java
@@ -0,0 +1,34 @@
+package com.example.workflow.event;
+
+import com.example.workflow.Taxa;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.web.client.RestTemplate;
+
+public class ValorTotalJurosMesDelegate implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+        String estado = execution.getVariable("estado").toString();
+        long principal = (long) execution.getVariable("valor");
+        double taxa = taxaJurosPorEstado(estado);
+        long meses = (long) execution.getVariable("quantidademeses");
+
+        double montante = principal * Math.pow((1 + taxa), meses);
+        double juros = montante - principal;
+
+        execution.setVariable("taxade_" + estado, taxa);
+        execution.setVariable("valortotal", String.format("%1$.2f", montante));
+        execution.setVariable("valorjuros", String.format("%1$.2f", juros));
+    }
+
+    public double taxaJurosPorEstado(String estado) {
+
+        RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
+        RestTemplate restTemplate = restTemplateBuilder.build();
+
+        Taxa entity = restTemplate.getForObject("http://localhost:9999/" + estado, Taxa.class);
+        return entity.getValor();
+    }
+
+}
